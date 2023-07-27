4187795cbcfdfdaadfaad9a31367583c519831d2
kiyohome <kiyohito.itoh@gmail.com>
2023-07-28 01:36:40 +0900

    Fix to not include dependent libraries

--- WebScripts~/rollup.config.ts
+++ WebScripts~/rollup.config.ts
@@ -1,26 +1,25 @@
 import typescript from "@rollup/plugin-typescript";
 import terser from "@rollup/plugin-terser";
-import { nodeResolve } from "@rollup/plugin-node-resolve";
 import { RollupOptions } from "rollup";
 
 const isProd = process.env.BUILD === "production";
 
 const config: RollupOptions = {
     input: "src/index.ts",
+    external: [
+        "@extreal-dev/extreal.integration.web.common",
+        "socket.io-client"
+    ],
     output: {
         file: "dist/index.js",
         format: "es",
         plugins: isProd ? [terser()] : [],
-        globals: {},
     },
     plugins: [
         typescript({
             declaration: true,
             declarationDir: "types",
             exclude: "rollup.config.ts"
-        }),
-        nodeResolve({
-            browser: true,
         }),
     ],
 };
