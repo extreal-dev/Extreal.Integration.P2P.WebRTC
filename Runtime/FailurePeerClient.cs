66ee7d97c9d842c27abed82392c7e21b6cfc7feb
Takuya Okita <takuya10@gmail.com>
2023-12-05 10:04:04 +0900

    Add a class that always returns an exception

---
+++ Runtime/FailurePeerClient.cs
@@ -0,0 +1,17 @@
+﻿#if !UNITY_WEBGL || UNITY_EDITOR
+namespace Extreal.Integration.P2P.WebRTC
+{
+    public class FailurePeerClient
+    {
+        /// <summary>
+        /// Creates NativeTextChatClient with peerClient.
+        /// </summary>
+        /// <param name="peerClient">Peer client.</param>
+        public FailurePeerClient(NativePeerClient peerClient)
+        {
+            peerClient.AddPcCreateHook((id, isOffer, pc) => throw new System.Exception("PeerClient Error Test"));
+            peerClient.AddPcCloseHook(id => throw new System.Exception("PeerClient Error Test"));
+        }
+    }
+}
+#endif
