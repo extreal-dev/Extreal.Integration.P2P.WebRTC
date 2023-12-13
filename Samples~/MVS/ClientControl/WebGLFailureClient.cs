351b25c9b116b3cfb8283e734c25c1c701dba2c4
Takuya Okita <takuya10@gmail.com>
2023-12-13 18:21:38 +0900

    Fix error in sample

--- Samples~/MVS/ClientControl/WebGLFailureClient.cs
+++ Samples~/MVS/ClientControl/WebGLFailureClient.cs
@@ -5,7 +5,7 @@ namespace Extreal.Integration.P2P.WebRTC.MVS.ClientControl
 {
     public class WebGLFailureClient
     {
-        public static FailureConnect() => WebGLHelper.CallAction("failure");
+        public static void FailureConnect() => WebGLHelper.CallAction("failure");
     }
 }
 #endif
