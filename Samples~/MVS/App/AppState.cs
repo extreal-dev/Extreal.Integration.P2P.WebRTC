a5f5ce5b5f60a0e4760fc87a5be42f03c85dc25c
sus-yoshikane-t <yoshikane-t@m.sus-g.co.jp>
2023-12-21 15:49:45 +0900

    Modify socket id set event

--- Samples~/MVS/App/AppState.cs
+++ Samples~/MVS/App/AppState.cs
@@ -9,9 +9,9 @@ namespace Extreal.Integration.P2P.WebRTC.MVS.App
         public IObservable<string> OnNotificationReceived => onNotificationReceived.AddTo(disposables);
         private readonly Subject<string> onNotificationReceived = new Subject<string>();
 
-        public IObservable<string> OnSocketIdSet => socketId.AddTo(disposables);
-        private readonly ReactiveProperty<string> socketId = new ReactiveProperty<string>();
-        public void SetSocketId(string socketId) => this.socketId.Value = socketId;
+        public IObservable<string> OnSocketIdSet => onSocketIdSet.AddTo(disposables);
+        private readonly Subject<string> onSocketIdSet = new Subject<string>();
+        public void SetSocketId(string socketId) => onSocketIdSet.OnNext(socketId);
 
         private readonly CompositeDisposable disposables = new CompositeDisposable();
 
